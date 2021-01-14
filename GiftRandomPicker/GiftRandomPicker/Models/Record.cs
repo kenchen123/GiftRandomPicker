f649ca49474a17b82f12c6263dc5af5c8d8e7544
Ken Chen <kenchen@equuscs.com>
2021-01-14 12:16:39 +0800

    Auto saving

---
+++ GiftRandomPicker/GiftRandomPicker/Models/Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace GiftRandomPicker.Models
+{
+    public class RecordTable
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Record { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+
+    public class Record
+    {
+        public string CurrentName { get; set; }
+        public string CurrentGiftNumber { get; set; }
+        public List<string> Employees { get; set; }
+        public List<int> Gifts { get; set; }
+    }
+}
